Remove normalization fixes output

Models/AutoencoderMNIST.py
@@ -37,7 +37,7 @@ class AutoencoderMNIST(nn.Module):
         x = F.relu(self.d_fc1(z))
         x = F.relu(self.d_fc2(x))
         x = F.relu(self.d_fc3(x))
-        x = F.relu(self.d_fc4(x))
+        x = self.d_fc4(x) # No ReLU on last laye: Sigmoid(ReLU(x)) learns to be either 0.5 or 1 exactly.
         x = self.d_sig(x)
         return x
 

ae_mnist_interactive_latent.py
@@ -29,18 +29,18 @@ batch_size_test = 1000
 trainLoader = torch.utils.data.DataLoader(
 	torchvision.datasets.MNIST('./data', train=True, download=True,
 		transform=torchvision.transforms.Compose([
-			torchvision.transforms.ToTensor(),
-			torchvision.transforms.Normalize(
-				(0.1307,), (0.3081,)) # Precomputed statistic for normalization
+			torchvision.transforms.ToTensor()
+			#torchvision.transforms.Normalize(
+		#		(0.1307,), (0.3081,)) # Precomputed statistic for normalization
 		])),
 	batch_size=batch_size, shuffle=True)
 
 testLoader = torch.utils.data.DataLoader(
 	torchvision.datasets.MNIST('./data', train=False, download=True,
 		transform=torchvision.transforms.Compose([
-			torchvision.transforms.ToTensor(),
-			torchvision.transforms.Normalize(
-				(0.1307,), (0.3081,))
+			torchvision.transforms.ToTensor()
+			#torchvision.transforms.Normalize(
+			#	(0.1307,), (0.3081,))
 		])),
 	batch_size=batch_size_test, shuffle=True)
 
@@ -81,6 +81,7 @@ if os.path.exists('./checkpoints/model.pt'):
 
 	exit()
 
+
 num_epochs = 5
 
 
@@ -94,7 +95,8 @@ def checkpoint(loss, model):
 model = Autoencoder().cuda()
 
 # We are using a Sigmoid layer at the end so we must use CE loss. Why?
-lossFun = nn.MSELoss()#nn.CrossEntropyLoss()
+# ---> Rather, paper said to use CE loss.
+lossFun = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), weight_decay=1e-5)
 
 